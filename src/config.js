d6e5659377483381aba8bc222c8365bc43bff60a
EmJoHig <67467333+EmJoHig@users.noreply.github.com>
2024-07-16 23:06:53 -0300

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-// import { config } from "dotenv";
-import pkg from 'dotenv';
-const { config } = pkg;
-config();
-
-export const PORT = process.env.PORT || 4000;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/tecnocleandb";
